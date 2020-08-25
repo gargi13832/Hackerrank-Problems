04cae229e36af3a6308a826a2850a4c76794c773
Gargi <gargi.kadam17@vit.edu>
2020-08-25 12:24:10 +0530

    smart-number

---
+++ Problem Solving/smart_number.cpp
@@ -0,0 +1,31 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+bool is_smart_number(int num) {
+    int val = (int) sqrt(num);
+    cout<<val<<" "<<num<<endl;
+    cout<<val*val<<" ";
+    if((num - (val*val)) == 0)
+        return true;
+    return false;
+}
+
+int main() {
+
+    int test_cases;
+    cin >> test_cases;
+    int num;
+    for(int i = 0; i < test_cases; i++) {
+        cin >> num;
+        bool ans = is_smart_number(num);
+        if(ans) {
+            cout << "YES" << endl;
+        }
+        else cout << "NO" << endl;
+    }
+    return 0;
+}
