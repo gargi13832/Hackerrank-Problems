a098f91fc56c4fe752700cae24a3cb898be021dc
Gargi <gargi.kadam17@vit.edu>
2020-08-26 14:57:35 +0530

    basic-data-types

---
+++ C++/c-tutorial-basic-data-types.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main()
+{
+    int x;
+    long t;
+    char a;
+    float m;
+    double z;
+    cin>>x>>t>>a>>m>>z;
+    cout<<x<<endl<<t<<endl<<a<<endl;
+    printf("%.03f\n",m);
+    printf("%.09lf\n",z);
+    return 0;
+}
+
+
