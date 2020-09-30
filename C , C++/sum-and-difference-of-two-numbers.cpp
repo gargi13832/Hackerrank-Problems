26ded4ae3c8b351b0f61201b37d84f2bcd59feb0
gargi13832 <you@example.com>
2020-10-01 01:19:55 +0530

    sum and difference of two numbers

---
+++ C , C++/sum-and-difference-of-two-numbers.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a,b;
+    scanf("%d%d",&a,&b);
+    float c,d;
+    scanf("%f%f",&c,&d);
+    printf("%d %d",a+b,a-b);
+    printf("\n%1.1f %1.1f",c+d,c-d);
+
+
+    return 0;
+}
+
+
