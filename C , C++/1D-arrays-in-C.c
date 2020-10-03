0ea9bfa8862caba5aa092c3c80cbad0e94832ad9
gargi13832 <you@example.com>
2020-10-04 01:26:25 +0530

    1D arrays in C

---
+++ C , C++/1D-arrays-in-C.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int arr[n],i,sum=0;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+        sum=sum+arr[i];
+    }
+    printf("%d",sum);
+    return 0;
+}
+
