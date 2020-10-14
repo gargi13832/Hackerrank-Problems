5adc02a39199295342e06465dbea05ebafbbfedc
gargi13832 <you@example.com>
2020-10-14 11:49:59 +0530

    calculating nth term

---
+++ C , C++/calculating_the_nth_term.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+//Complete the following function.
+
+int find_nth_term(int n, int a, int b, int c)
+{
+    int x=0,i;
+    for(i=3;i<n;i++)
+    {
+        x=b+c;
+        b=c;
+        c=x;
+    }
+    return x;
+}
+
+int main() {
+    int n, a, b, c;
+
+    scanf("%d %d %d %d", &n, &a, &b, &c);
+    int ans = find_nth_term(n, a, b, c);
+
+    printf("%d", ans);
+    return 0;
+}
+
+
